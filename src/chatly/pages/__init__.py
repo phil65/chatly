c6ecd13e49cb96ab9d810dda8135500252a94e7f
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-02 01:33:33 +0200

    chore: prep documents stuff

--- src/chatly/pages/__init__.py
+++ src/chatly/pages/__init__.py
@@ -5,12 +5,14 @@ from chatly.pages.ocr_page import OcrPage
 from chatly.pages.start_page import StartPage
 from chatly.pages.chat_page import ChatPage
 from chatly.pages.browser_page import HelpPage
+from chatly.pages.documents_page import DocumentsPage
 
 # from chatly.pages.explorer_page import FileExplorerPage
 from chatly.pages.log_page import LogPage
 
 __all__ = [
     "ChatPage",
+    "DocumentsPage",
     # "FileExplorerPage",
     "HelpPage",
     "LogPage",
