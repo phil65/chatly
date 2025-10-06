738cc343670042cf451b0dce3bf43a1afb5195bb
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-06 18:00:14 +0200

    chore: bump template

--- src/chatly/__init__.py
+++ src/chatly/__init__.py
@@ -1,9 +1,18 @@
+"""Chatly: Agent desktop app"""
+
 from __future__ import annotations
 
 from importlib.metadata import version
 
 __version__ = version("chatly")
 __title__ = "Chatly"
+__description__ = "Agent desktop app"
+__author__ = "Philipp Temminghoff"
+__author_email__ = "philipptemminghoff@googlemail.com"
+__copyright__ = "Copyright (c) 2025 Philipp Temminghoff"
+__license__ = "MIT"
+__url__ = "https://github.com/phil65/chatly"
+
 
 from chatly.application import MainApp
 
